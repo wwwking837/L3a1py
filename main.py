5f707f55d3c4d7517b93bc2c05bbcc9fbf75bcab
wwwking837 <muhammadzaroonadnan@gmail.com>
2025-02-11 18:19:54 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+number = int(input("Enter Number to be checked"))
+
+print("Number to be checked :", number)
+
+if number%2==0 :
+    print("This is a even number")
+
+else:
+    print("this is a odd number")
